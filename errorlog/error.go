fc4c3b8cf6fb4cfd575a826eef2e664afd98efd6
Tim Tosi <timothee.tosi@gmail.com>
2016-05-04 19:38:06 +0200

    add Error func

---
+++ errorlog/error.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// -----------------------------------------------------------------------------
+
+// Error returns an error formated according to GNU's error message standard.
+func Error(err error) error {
+	_, file, line, _ := runtime.Caller(0)
+	return fmt.Errorf("%s:%d: %s", file, line, err)
+}
