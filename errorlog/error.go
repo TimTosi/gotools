46fd0f71a129fec054629458276000927f41ee17
Tim Tosi <timothee.tosi@gmail.com>
2016-06-06 17:16:06 +0200

    errorlog update caller arg

--- errorlog/error.go
+++ errorlog/error.go
@@ -9,6 +9,6 @@ import (
 
 // Error returns an error formated according to GNU's error message standard.
 func Error(err error) error {
-	_, file, line, _ := runtime.Caller(0)
+	_, file, line, _ := runtime.Caller(1)
 	return fmt.Errorf("%s:%d: %s", file, line, err)
 }
