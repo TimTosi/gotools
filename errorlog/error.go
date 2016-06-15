dca0e25eee7cfef0871c5b447577cd515a1c158c
Tim Tosi <timothee.tosi@gmail.com>
2016-06-15 17:15:47 +0200

    bugfix

--- errorlog/error.go
+++ errorlog/error.go
@@ -9,6 +9,9 @@ import (
 
 // Error returns an error formated according to GNU's error message standard.
 func Error(err error) error {
-	_, file, line, _ := runtime.Caller(1)
-	return fmt.Errorf("%s:%d: %s", file, line, err)
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		return fmt.Errorf("%s:%d: %s", file, line, err)
+	}
+	return nil
 }
