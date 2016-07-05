b9992f41b7036fd07237010b039010694ae20e6b
Tim Tosi <timothee.tosi@gmail.com>
2016-07-05 13:55:19 +0200

    docs(queue): add documentation

--- network/queue/queue.go
+++ network/queue/queue.go
@@ -7,7 +7,10 @@ import (
 
 // -----------------------------------------------------------------------------
 
-// TODO
+// Shift returns the `queue.Message` contained at the index 0 of `q.msgs` if
+// the length of `q.msgs` is greater than 0. It returns `nil` otherwise.
+//
+// NOTE: This function is thread-safe.
 func (q Queue) Shift() *Message {
 	q.Lock()
 	defer q.Unlock()
@@ -20,7 +23,9 @@ func (q Queue) Shift() *Message {
 	return nil
 }
 
-// TODO
+// Push appends `msg` at the end of `q.msgs`.
+//
+// NOTE: This function is thread-safe.
 func (q Queue) Push(msg *Message) {
 	q.Lock()
 	defer q.Unlock()
@@ -28,22 +33,28 @@ func (q Queue) Push(msg *Message) {
 	q.msgs = append(q.msgs, msg)
 }
 
-// TODO
-// launched with go
-// Specify that it is a long running routine
+// Poll purges `q.msgs` from `queue.Message` no longer relevant.
+// In the case where an `ID` is received on `IDChan`, the corresponding
+// `queue.Message` is deleted from `q.msgs` and `q.Purge()` is called.
+// When no `ID` is received on `IDChan` after `d` time, `q.Purge()` is called.
+//
+// NOTE: This function is an infinite loop.
 func (q Queue) Poll(IDChan <-chan int, msgChan chan<- *Message, d time.Duration) {
 	for {
 		select {
 		case ID := <-IDChan:
 			q.Discard(ID)
+			msgChan <- q.Purge(d)
 		case <-time.After(d):
 			msgChan <- q.Purge(d)
 		}
 	}
 }
 
-// TODO
-// NOTE: No leak
+// Discard deletes the first `queue.Message` contained in `q.msgs` where
+// `queue.Message.ID` matches `ID`.
+//
+// NOTE: This function is thread-safe.
 func (q Queue) Discard(ID int) bool {
 	q.Lock()
 	defer q.Unlock()
@@ -59,13 +70,14 @@ func (q Queue) Discard(ID int) bool {
 	return false
 }
 
-// TODO
-// NOTE: No leak
-// TODO BETTER LOCKING HERE
+// Purge deletes all `queue.Message`s from `q.msgs` where the time elapsed
+// since `queue.Message.Timeout` is greater or equal to `d`.
+//
+// NOTE: This function is thread-safe.
 func (q Queue) Purge(d time.Duration) *Message {
 	q.Lock()
 	defer q.Unlock()
-
+	// TODO BETTER LOCKING HERE
 	for i := 0; i < len(q.msgs); i++ {
 		if q.msgs[i].TimeoutReached(d) == true {
 			msg := q.msgs[i]
@@ -78,7 +90,9 @@ func (q Queue) Purge(d time.Duration) *Message {
 	return nil
 }
 
-// TODO + explanation first in last out container
+// Queue is a structure representing a first-in first-out container used by
+// `queue.ZMQBroker` for re-emitting a given `queue.Message`s in the case
+// where a `queue.ZMQWorker` crashes while processing this `queue.Message`.
 type Queue struct {
 	*sync.RWMutex
 
