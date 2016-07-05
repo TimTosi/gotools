f4fc4d0b6990a42f2115f39d53daa7e9d710799b
Tim Tosi <timothee.tosi@gmail.com>
2016-07-05 17:01:48 +0200

    docs(worker): add documentation

--- network/queue/worker.go
+++ network/queue/worker.go
@@ -4,35 +4,39 @@ import zmq "github.com/pebbe/zmq4"
 
 // -----------------------------------------------------------------------------
 
-// TODO
+// Receive returns a `string` containing a message received on `w.soc` or
+// an error.
 func (w *ZMQWorker) Receive() (msg string, err error) {
 	w.soc.Send("", zmq.SNDMORE)
-	w.soc.Send("Hi Boss", 0) // Change msg
+	w.soc.Send("Hi Boss", 0) // Change msg and send last message ID received
 
 	if _, err := w.soc.Recv(0); err != nil {
-		return "", nil
+		return "", err
 	}
 	return w.soc.Recv(0)
 }
 
-// TODO
-func (w *ZMQWorker) Close() {
-	w.Close()
-}
+// Close releases resources acquired by `w.soc`.
+func (w *ZMQWorker) Close() { _ = w.soc.Close() }
 
-// TODO
+// Identity returns a `string` containing `w.soc`s identity or an `error`.
 func (w *ZMQWorker) Identity() (string, error) { return w.soc.GetIdentity() }
 
 // -----------------------------------------------------------------------------
 
-// TODO
+// ZMQWorker is a structure representing a worker process. The network
+// communication stack lies on a Go implementation of the ZeroMQ library.
 type ZMQWorker struct {
-	Worker
-
 	soc *zmq.Socket
 }
 
 // NewZMQWorker returns a new `ZMQWorker`.
+//
+// NOTE: `id` should be unique.
+// NOTE: `addr` must be of the form
+// - `tcp://<hostname>:<port>` for "regular" TCP networking.
+// - `inproc://<name>` for in-process networking.
+// - `ipc:///<tmp/filename>` for inter-process communication.
 func NewZMQWorker(addr, id string) (*ZMQWorker, error) {
 	soc, err := zmq.NewSocket(zmq.DEALER)
 	if err != nil {
